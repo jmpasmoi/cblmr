502bcd24cc56096df34afb1e2fe52fa16eb32b69
jmcimula <jmkacim@gmail.com>
2016-09-28 02:14:17 +0200

    Choice of Best Linear Model

--- R/cblmr.R
+++ R/cblmr.R
@@ -4,30 +4,38 @@
 #' This function allows you to choose the best model from among different "best" models.
 #' Evaluating based on predictions and goodness of fit.
 #'
-#' @param dataframe only numerical values are required (explanatory and response variables)
+#' @param dataframe only numerical values are required(explanatory and response variables)
 #' @param response  dependent variable
 #' @param exp.comb  number of expected combinations
+#' @param choice when choice=TRUE it return the best model based on exp.comb. Default choice=FALSE
 #'
 #' @return a tab grouping predictions and goodness of fit per model
 #' @author Jean Marie Cimula
 #' @details
-#' Evaluating based on predictions (PRESS : Prediction Sum of Squares) and
-#' goodness of fit (AIC, BIC, R Squared, Adj. Squared).
+#' Evaluating based on predictions(PRESS : Prediction Sum of Squares) and
+#' goodness of fit(AIC, BIC, R Squared, Adj. Squared).
 #'
 #' @export
 #'
 
 
-blm_choice <- function (dataframe, response, ..., exp.comb){
+blm_choice <- function(dataframe, response, exp.comb, ..., choice = FALSE){
+
+
 
 
   f <- formals(blm_choice)  # Capture formal arguments
 
   formalNames <- names(f)
 
-  exc <- do.call(missing, list(formalNames[4]))
+  exc <- do.call(missing, list(formalNames[3]))
+  chc <- do.call(missing, list(formalNames[5]))
 
+  if(chc == FALSE && exc == TRUE){
 
+    stop("Missing exp.comb as integer", call. = FALSE)
+
+  }
 
   if(exc == TRUE){ ec <- "abs"}
   else{
@@ -44,14 +52,14 @@ blm_choice <- function (dataframe, response, ..., exp.comb){
   #attach(input_unit)
 
   #(1) Number of colums of the data frame
-  getLen <- dim(input_unit)[2]
+  get_len <- dim(input_unit)[2]
 
-  for (i in 1:getLen){
+  for(i in 1:get_len){
 
     #Removing the response variable in the temporary data frame
-    if (names(input_unit)[i] == response){
+    if(names(input_unit)[i] == response){
 
-      expLayer <- input_unit [,-i]
+      exp_layer <- input_unit [,-i]
 
       break;
 
@@ -60,38 +68,38 @@ blm_choice <- function (dataframe, response, ..., exp.comb){
   }#Loop for
 
   #(2)Number of colums of the temporary data frame of explanatory variables
-  getLenExpVar <- dim(expLayer)[2]
+  get_len_expvar <- dim(exp_layer)[2]
 
   #(3)Initializing a Matrix which will contain all combinations of predictor model
-  ExpVarMatrix <- matrix( ncol = getLenExpVar)
+  exp_var_mat <- matrix( ncol = get_len_expvar)
 
   #Creating combination
-  for (i in 1:getLenExpVar){
+  for(i in 1:get_len_expvar){
     #Combination function
-    comb      <- t(combn(names(expLayer),i))
-    numbRow   <- nrow(comb)
-    numbCol   <- length(names(expLayer))
-    numbRowNA <- numbRow
-    numbColNA <- numbCol-ncol(comb)
-    naMatr    <- matrix(rep(NA, numbRowNA*numbColNA), nrow = numbRowNA, ncol = numbColNA)
-    result    <- cbind(comb, naMatr)
-    ExpVarMatrix <- rbind(ExpVarMatrix, result)
+    comb      <- t(combn(names(exp_layer),i))
+    nb_row   <- nrow(comb)
+    nb_col   <- length(names(exp_layer))
+    nb_row_na <- nb_row
+    nb_col_na <- nb_col-ncol(comb)
+    na_matr    <- matrix(rep(NA, nb_row_na*nb_col_na), nrow = nb_row_na, ncol = nb_col_na)
+    result    <- cbind(comb, na_matr)
+    exp_var_mat <- rbind(exp_var_mat, result)
 
   }#Loop for
   #Removing all NA
-  ExpVarMatrix <- ExpVarMatrix[-1,]
+  exp_var_mat <- exp_var_mat[-1,]
 
   #Final result of combination between response and explanatory variables
   #Setting an empty data frame
   make_your_choice <- data.frame()
 
-  for (i in 1:nrow(ExpVarMatrix)){
+  for(i in 1:nrow(exp_var_mat)){
 
-    getVal    <- na.omit (ExpVarMatrix[i, ])
+    getval    <- na.omit(exp_var_mat[i, ])
 
 
 
-    mdRegComb <- paste (response, " ~ ", paste (getVal, collapse = " + "), sep = "")
+    mdRegComb <- paste(response, " ~ ", paste(getval, collapse = " + "), sep = "")
 
     #print(mdRegComb)
     mdLM <- lm(as.formula(mdRegComb),data=input_unit)
@@ -115,7 +123,7 @@ blm_choice <- function (dataframe, response, ..., exp.comb){
     ElasticNet <- 0
     Cp <- 0
 
-    if (stringr::str_detect(mdRegComb,'[+]') == TRUE){
+    if(stringr::str_detect(mdRegComb,'[+]') == TRUE){
 
       #Checking the tilde in the model
       delim   <- unlist(gregexpr(pattern ='[~]',mdRegComb)) - 1
@@ -129,8 +137,8 @@ blm_choice <- function (dataframe, response, ..., exp.comb){
       #Retrieving the second part with the exploratory variables
       sndPart <- substr(mdRegComb,delim + 2, stringr::str_length(mdRegComb))
 
-      nbResp  <- getNumberResponse (input_unit,stPart) #Column of Response variable
-      nbExp   <- getNumberExploratory (input_unit,sndPart)#Columns of Exploratory variables
+      nbResp  <- getNumberResponse(input_unit,stPart) #Column of Response variable
+      nbExp   <- getNumberExploratory(input_unit,sndPart)#Columns of Exploratory variables
       Ridge   <- getRidgeValue(input_unit,nbResp,nbExp)
       Lasso   <- getLassoValue(input_unit,nbResp,nbExp)
       ElasticNet <- getElasticNetValue(input_unit,nbResp,nbExp)
@@ -139,7 +147,7 @@ blm_choice <- function (dataframe, response, ..., exp.comb){
       }
 
     #Assembling diagnostic parameters per model predictor in Matrix of all combinations
-    dFrame <- data.frame(modelReg = mdRegComb,RSquared = RSqrt,AdjustedRSquared = AdjRSqrt,AIC = AIC,BIC = BIC,PRESS = PL,Ridge = Ridge,Lasso = Lasso, ElasticNet = ElasticNet,Cp=Cp, nexp = length(getVal))#, nbResp = nbResp, nbExp = nbExp)#, Accuracy = RidgeRegression)
+    dFrame <- data.frame(modelReg = mdRegComb,RSquared = RSqrt,AdjustedRSquared = AdjRSqrt,AIC = AIC,BIC = BIC,PRESS = PL,Ridge = Ridge,Lasso = Lasso, ElasticNet = ElasticNet,Cp=Cp, nexp = length(getval))#, nbResp = nbResp, nbExp = nbExp)#, Accuracy = RidgeRegression)
 
     #Loading data frame
     make_your_choice <- rbind(make_your_choice,dFrame)
@@ -157,20 +165,39 @@ blm_choice <- function (dataframe, response, ..., exp.comb){
     }
 
     rownames(df) <-  NULL
+    colnames(df)[2] <- "r_squared"
+    colnames(df)[3] <- "adj_r_squared"
+
+    if(choice){
+
+      df <- sqldf::sqldf("
+             select * from df
+                      where r_squared = (select max(r_squared) from df)
+                            and adj_r_squared = (select max(adj_r_squared) from df)
+                            and Cp = (select max(Cp) from df)
+                            and AIC = (select min(AIC) from df)
+                            and BIC = (select min(BIC) from df)
+                            and PRESS = (select min(PRESS) from df)
+                            and Ridge = (select min(Ridge) from df)
+                            and Lasso = (select min(Lasso) from df)
+                            and ElasticNet = (select min(ElasticNet) from df)
+            ")
+
+    }
 
     knitr::kable(df)
 
 }#End function
 
-getNumberResponse <- function (dataframe, model){
+getNumberResponse <- function(dataframe, model){
 
   #Retrieving the number of the column of the response variable
 
   colData <- 0
 
-  for (j in 1:dim(dataframe)[2]){
+  for(j in 1:dim(dataframe)[2]){
     #Take one by one chosen exploratory variables
-    if (names(dataframe)[j] == stringr::str_trim(model)){
+    if(names(dataframe)[j] == stringr::str_trim(model)){
 
          colData <- j
          break;
@@ -187,7 +214,7 @@ getNumberResponse <- function (dataframe, model){
 
   }#End function
 
-getNumberExploratory <- function (dataframe, model){
+getNumberExploratory <- function(dataframe, model){
 
   ChPlusLength <- length(unlist(gregexpr(pattern = "[+]",model))) + 1 #The Length of the character + in the string augmented of 1
 
@@ -196,14 +223,14 @@ getNumberExploratory <- function (dataframe, model){
   getColData   <- NULL
 
   #Retrieving the number of columns for all explanatory variables
-  for (i in 1 : length(chPlus)){
+  for(i in 1 : length(chPlus)){
 
     H <- stringr::str_trim(chPlus[i]) #Decomposing the exploratory variable one by one from Linear Model presentation
 
-    for (j in 1:dim(dataframe)[2]){
+    for(j in 1:dim(dataframe)[2]){
 
       #Take one by one chosen exploratory variables
-      if (names(dataframe)[j] == H){
+      if(names(dataframe)[j] == H){
 
              colData <- j
              break;
@@ -227,13 +254,13 @@ getRidgeValue <- function(dataframe,nbResp,nbExp){
   nbExp <- substr(nbExp,2,stringr::str_length(nbExp))
 
   #Taking the element of the pair one by one
-  nbExp <- stringr::str_split_fixed (nbExp,",", n = length(unlist(gregexpr(pattern = ",",nbExp))) + 1 )
+  nbExp <- stringr::str_split_fixed(nbExp,",", n = length(unlist(gregexpr(pattern = ",",nbExp))) + 1 )
 
   dFrame <- data.frame(row.names=1:nrow(dataframe))
 
   n <- 0
 
-  for (i in 1 : length(nbExp)){
+  for(i in 1 : length(nbExp)){
 
     n <- as.integer(nbExp[i])
 
@@ -262,7 +289,7 @@ getRidgeValue <- function(dataframe,nbResp,nbExp){
   rmse <- mean((y - predictions)^2)
 
   #return
-  return (rmse)
+  return(rmse)
 
 }#End-function
 
@@ -272,13 +299,13 @@ getLassoValue <- function(dataframe,nbResp,nbExp){
   nbExp <- substr(nbExp,2,stringr::str_length(nbExp))
 
   #Taking the element of the pair one by one
-  nbExp <- stringr::str_split_fixed (nbExp,",", n = length(unlist(gregexpr(pattern = ",",nbExp))) + 1 )
+  nbExp <- stringr::str_split_fixed(nbExp,",", n = length(unlist(gregexpr(pattern = ",",nbExp))) + 1 )
 
   dFrame <- data.frame(row.names=1:nrow(dataframe))
 
   n <- 0
 
-  for (i in 1 : length(nbExp)){
+  for(i in 1 : length(nbExp)){
 
     n <- as.integer(nbExp[i])
 
@@ -310,7 +337,7 @@ getLassoValue <- function(dataframe,nbResp,nbExp){
   rmse <- mean((y - predictions)^2)
 
   #return
-  return (rmse)
+  return(rmse)
 
 }#End-function
 
@@ -320,13 +347,13 @@ getElasticNetValue <- function(dataframe,nbResp,nbExp){
   nbExp <- substr(nbExp,2,stringr::str_length(nbExp))
 
   #Taking the element of the pair one by one
-  nbExp <- stringr::str_split_fixed (nbExp,",", n = length(unlist(gregexpr(pattern = ",",nbExp))) + 1 )
+  nbExp <- stringr::str_split_fixed(nbExp,",", n = length(unlist(gregexpr(pattern = ",",nbExp))) + 1 )
 
   dFrame <- data.frame(row.names=1:nrow(dataframe))
 
   n <- 0
 
-  for (i in 1 : length(nbExp)){
+  for(i in 1 : length(nbExp)){
 
     n <- as.integer(nbExp[i])
 
@@ -353,23 +380,23 @@ getElasticNetValue <- function(dataframe,nbResp,nbExp){
   rmse <- mean((y - predictions)^2)
 
   #return
-  return (rmse)
+  return(rmse)
 
 }#End-function
 
-getMallowCp <- function (dataframe,nbResp,nbExp){
+getMallowCp <- function(dataframe,nbResp,nbExp){
 
   #Putting the exploratory in the vector
   nbExp <- substr(nbExp,2,stringr::str_length(nbExp))
 
   #Taking the element of the pair one by one
-  nbExp <- stringr::str_split_fixed (nbExp,",", n = length(unlist(gregexpr(pattern = ",",nbExp))) + 1 )
+  nbExp <- stringr::str_split_fixed(nbExp,",", n = length(unlist(gregexpr(pattern = ",",nbExp))) + 1 )
 
   dFrame <- data.frame(row.names=1:nrow(dataframe))
 
   n <- 0
 
-  for (i in 1 : length(nbExp)){
+  for(i in 1 : length(nbExp)){
 
     n <- as.integer(nbExp[i])
 
